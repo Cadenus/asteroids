1bcb5fd4411ee1c8d736c54b510ce6a1b926eb85
Christian Kullmann <dev@christiankullmann.de>
2025-06-25 10:20:59 +0200

    Added Player

--- main.py
+++ main.py
@@ -3,23 +3,27 @@
 # throughout this file
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     timeclock = pygame.time.Clock()
     dt = 0
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
         dt = timeclock.tick(60) / 1000
 
