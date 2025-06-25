0fdc7cd74070a6f85c6e64fcb4b9d558e008352d
Christian Kullmann <dev@christiankullmann.de>
2025-06-25 10:25:50 +0200

    Added rotation control on A and D

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -18,6 +18,15 @@ class Player(CircleShape):
         
     def draw(self, screen):
         triangle = self.triangle()
-        # print(f"Screen: {screen}")
-        # print(f"triangle: {triangle}")
         pygame.draw.polygon(screen,"white", self.triangle(), 1)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
